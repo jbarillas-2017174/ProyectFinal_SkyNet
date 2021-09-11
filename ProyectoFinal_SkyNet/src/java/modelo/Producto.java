b4343d1499c308881d0297cd711f139bb840d5d2
byron figueroa <bfigueroa-2020308@kinal.edu.gt>
2021-09-11 11:35:32 -0600

    Se termino de editar el modelo Producto

--- ProyectoFinal_SkyNet/src/java/modelo/Producto.java
+++ ProyectoFinal_SkyNet/src/java/modelo/Producto.java
@@ -5,10 +5,63 @@
  */
 package modelo;
 
-/**
- *
- * @author ZEA
- */
 public class Producto {
+    private int codigoProducto ;
+    private String nombresProducto;
+    private double precioProducto;
+    private int stock;
+    private String estadoProducto;
+
+    public Producto() {
+    }
+
+    public Producto(int codigoProducto, String nombresProducto, double precioProducto, int stock, String estadoProducto) {
+        this.codigoProducto = codigoProducto;
+        this.nombresProducto = nombresProducto;
+        this.precioProducto = precioProducto;
+        this.stock = stock;
+        this.estadoProducto = estadoProducto;
+    }
+
+    public int getCodigoProducto() {
+        return codigoProducto;
+    }
+
+    public void setCodigoProducto(int codigoProducto) {
+        this.codigoProducto = codigoProducto;
+    }
+
+    public String getNombresProducto() {
+        return nombresProducto;
+    }
+
+    public void setNombresProducto(String nombresProducto) {
+        this.nombresProducto = nombresProducto;
+    }
+
+    public double getPrecioProducto() {
+        return precioProducto;
+    }
+
+    public void setPrecioProducto(double precioProducto) {
+        this.precioProducto = precioProducto;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getEstadoProducto() {
+        return estadoProducto;
+    }
+
+    public void setEstadoProducto(String estadoProducto) {
+        this.estadoProducto = estadoProducto;
+    }
+    
     
 }
