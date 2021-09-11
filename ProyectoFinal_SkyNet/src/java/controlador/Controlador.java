3e6b4457d338dbed24f5477c0b83de560a71e088
ecastellanos-2017333 <ecastellanos-2017333@kinal.edu.gt>
2021-09-11 17:55:33 -0600

    Se terminó de editar el Controlar

--- ProyectoFinal_SkyNet/src/java/controlador/Controlador.java
+++ ProyectoFinal_SkyNet/src/java/controlador/Controlador.java
@@ -29,18 +29,9 @@ public class Controlador extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Controlador</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Controlador at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        String menu = request.getParameter("menu");
+        if(menu.equals("Principal")){
+            request.getRequestDispatcher("inicio.jsp").forward(request, response);
         }
     }
 
